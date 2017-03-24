bdb47389dc8b7d722dd69442255a10d87d624f12
insu <insu@DESKTOP-U18FBB9>
2017-03-24 09:51:37 +0900

    실습과제4

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -29,11 +29,14 @@ public class CarTest {
 		car2.setModel("K7");
 		car2.setColor("흰색");
 		car2.setMaxSpeed(246);
+		car2.setPrice(40000000);
 		
 		Car car3 = new Car();
 		car3.setCompany("삼성자동차");
 		car3.setModel("SM7");
 		car3.setColor("회색");
+		car3.setMaxSpeed(200);
+		car3.setPrice(38000000);
 		
 		System.out.println("<< 자동차 목록 >>");
 		System.out.println("제조사명 : " + car1.getCompany());
@@ -46,13 +49,13 @@ public class CarTest {
 		System.out.println("모델명 : " + car2.getModel());
 		System.out.println("색상 : " + car2.getColor());
 		System.out.println("최대속도 : " + car2.getMaxSpeed()+"km");
-		System.out.println("가격 : " + car2.getPrice()+"원");
+		System.out.printf("가격 : %,d원\n" ,car2.getPrice());
 		System.out.println();
 		System.out.println("제조사명 : " + car3.getCompany());
 		System.out.println("모델명 : " + car3.getModel());
 		System.out.println("색상 : " + car3.getColor());
 		System.out.println("최대속도 : " + car3.getMaxSpeed()+"km");
-		System.out.println("가격 : " + car3.getPrice()+"원");
+		System.out.printf("가격 : %,d원\n" , car3.getPrice());
 		
 	}
 }
